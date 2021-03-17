8727ad4a521084bec095cad1de566ad67855ae61
Robbin Ma <maluqian1127@gmail.com>
2021-03-17 23:48:18 +0800

    add folder picker.
    Signed-off-by: Robbin Ma <maluqian1127@gmail.com>

--- lib/view/operation.dart
+++ lib/view/operation.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'package:file_picker_cross/file_picker_cross.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
 
 class OperationView extends StatefulWidget {
@@ -29,6 +32,41 @@ class _OperationPageState extends State<OperationPage> {
         actions: [TextButton(onPressed: () {}, child: Text('reset'))],
         elevation: 0,
       ),
+      body: OperationHome(),
+    );
+  }
+}
+
+class OperationHome extends StatefulWidget {
+  @override
+  _OperationHomeState createState() => _OperationHomeState();
+}
+
+class _OperationHomeState extends State<OperationHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          '提示：请将待检测人员和待检测数据库分别放置在两个文件夹中，以便于选择。',
+          style: TextStyle(color: Colors.red),
+        ),
+        Row(
+          children: [
+            TextButton(
+                onPressed: () async {
+                  FilePickerCross myFile =
+                      await FilePickerCross.importFromStorage(
+                          type: FileTypeCross.any,
+                          // Available: `any`, `audio`, `image`, `video`, `custom`. Note: not available using FDE
+                          fileExtension:
+                              'txt, md' // Only if FileTypeCross.custom . May be any file extension like `dot`, `ppt,pptx,odp`
+                          );
+                },
+                child: Text('选择文件夹'))
+          ],
+        )
+      ],
     );
   }
 }
